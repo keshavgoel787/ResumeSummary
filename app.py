1c59e05ee38d038c65797c48e26a01394f6806b9
keshavgoel787 <kgoel9657@gmail.com>
2024-07-29 10:36:36 -0400

    File upload function

--- app.py
+++ app.py
@@ -5,7 +5,21 @@ from tempfile import NamedTemporaryFile
 app = FastAPI()
 
 @app.post("/transcribe/")
+async def transcribe_audio(file: UploadFile = File(...)):
+    try:
+        # Save the uploaded file
+        file_location = f"temp/{file.filename}"
+        with open(file_location, "wb+") as file_object:
+            file_object.write(file.file.read())
+
+        #returns transcription
+        return JSONResponse(content={"transcription": file_location})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 @app.get("/", response_class=RedirectResponse)
 async def redirect_to_docs():
     return "/docs"
+
+
