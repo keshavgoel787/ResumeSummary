382d12ef0b1455497245374bd9b251d00c668610
keshavgoel787 <kgoel9657@gmail.com>
2024-07-31 11:36:10 -0400

    Debug

--- app.py
+++ app.py
@@ -87,17 +87,17 @@ async def transcribe_audio(filekey: str):
         if extracted is None:
             raise HTTPException(status_code=500, detail="Failed to extract text from PDF")
 
-        """"
+        
         insights = summarize_resume(extracted)
         if insights is None:
             raise HTTPException(status_code=500, detail="Failed to summarize resume")
-        
+        """"
         questions = generate_project_questions(insights)
 
         client.put_object(Bucket=os.getenv('Bucket'), Key=filekey + ' Summary', Body=insights+questions)
         """
         # returns transcription
-        return JSONResponse(content={"transcription": extracted})
+        return JSONResponse(content={"transcription": insights})
 
 @app.get("/", response_class=RedirectResponse)
 async def redirect_to_docs():
