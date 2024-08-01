5c83ab74dfbb2f22f5860b3afbc2281d158fee6a
keshavgoel787 <kgoel9657@gmail.com>
2024-07-31 23:13:08 -0400

    Revert Test

--- app.py
+++ app.py
@@ -60,11 +60,28 @@ client = session.client('s3',
                         aws_secret_access_key=os.getenv('SECRET_ACCESS_KEY'))
 
 @app.post("/transcribe/")
-async def transcribe_audio():
-        response = model.generate_content("Hello what is your name")
-        response = str(response)
+async def transcribe_audio(filekey: str):
+    with TemporaryDirectory() as temp_dir:
+        file_location = os.path.join(temp_dir, filekey)
+        try:
+            client.download_file(os.getenv('Bucket'), filekey, file_location)
+        except (NoCredentialsError, PartialCredentialsError) as e:
+            raise HTTPException(status_code=403, detail="Credentials error")
+
+        extracted = extract_text_from_pdf(file_location)
+        if extracted is None:
+            raise HTTPException(status_code=500, detail="Failed to extract text from PDF")
+
+        
+        insights = summarize_resume(extracted)
+        if insights is None:
+            raise HTTPException(status_code=500, detail="Failed to summarize resume")
+      
+
+        #client.put_object(Bucket=os.getenv('Bucket'), Key=filekey + ' Summary', Body=insights+questions)
+
         # returns transcription
-        return JSONResponse(content={"transcription": response})
+        return JSONResponse(content={"transcription": insights})
 
 @app.get("/", response_class=RedirectResponse)
 async def redirect_to_docs():
